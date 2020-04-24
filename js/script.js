41a01f858ce1017e680c3457c9e0d237dac5852a
EC2 Default User <ec2-user@ip-172-31-45-59.ap-northeast-1.compute.internal>
2020-04-24 01:40:11 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(".navbar-brand").click(function(){
+  $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+})
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position},speed);
+    return false;
+  });
+});
+
+$("#first_picture").click(function(){
+  if($("#first_detail").css("display") == "none"){
+    $(".detail_content").css("display","none");
+    $("#first_detail").toggle();
+  } else {
+    $("#first_detail").css("display","none");
+  }
+});
+
+$("#second_picture").click(function(){
+  if($("#second_detail").css("display") == "none"){
+     $(".detail_content").css("display","none");
+  $("#second_detail").toggle();
+  } else {
+    $("#second_detail").css("display","none");
+  }
+});
+
+$("#third_picture").click(function(){
+  if($("#third_detail").css("display") == "none"){
+     $(".detail_content").css("display","none");
+  $("#third_detail").toggle();
+  } else {
+    $("#third_detail").css("display","none");
+  }
+});
